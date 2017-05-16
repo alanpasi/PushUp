4aba3b7c15c04bba2a85a4159fa21ec889b24c4c
alanpasi <alanpasi@gmail.com>
2017-05-16 15:44:52 -0300

    Initial commit

---
+++ app/src/main/java/com/alanpasi/pushup/MainActivity.java
@@ -0,0 +1,18 @@
+package com.alanpasi.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUpLoad() {
+        int i = 0;
+        i += i;
+    }
+}
